c81c90e0ec98efa6549971e73b124ccfefc287ba
Ajilalitha <ajilalitha010@gmail.com>
2024-12-13 18:20:05 +0530

    create the product route

--- backend/server.js
+++ backend/server.js
@@ -7,16 +7,19 @@ import tvRoutes from './routes/tv.route.js';
 
 import { ENV_VARS } from './config/envVars.js';
 import { connectDB } from './config/db.js';
+import cookieParser from 'cookie-parser';
+import { protectRoute } from './middleware/protectRoute.js';
 
 dotenv.config();
 const app = express();
 const PORT = ENV_VARS.PORT;
 
 app.use(express.json()); //will allow us to pars req.body
+app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/movie", movieRoutes);
-app.use("/api/v1/tv", tvRoutes);
+app.use("/api/v1/movie", protectRoute, movieRoutes);
+app.use("/api/v1/tv", protectRoute, tvRoutes);
 
 
 app.listen(PORT, ()=>{
--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -4,7 +4,7 @@ import { ENV_VARS } from '../config/envVars.js';
 export const generateTokenAndSetCookie = (userId, res) =>{
   const token = jwt.sign({userId}, ENV_VARS.JWT_SECRET, {expiresIn: "15 d"});
 
-  res.cookie("jwt-netfilx", token, {
+  res.cookie("jwt-netflix", token, {
     maxAge: 15 * 24 * 60 * 60 * 1000, //15 days in milliseconds
     httpOnly:true, //prevent Xss attacks cross-site scripting attacks,make it not be accessed by JS
     sameSite:"Strict", //CSRF attacks cross-site request forquery attacks
