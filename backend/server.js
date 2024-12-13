48d3f3e1929bf79ee895099cfc33f0bd6389b5da
Ajilalitha <ajilalitha010@gmail.com>
2024-12-13 15:47:02 +0530

    Get the all movie controller

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from "dotenv";
 import authRoutes from './routes/auth.route.js';
+import movieRoutes from './routes/movie.route.js';
 import { ENV_VARS } from './config/envVars.js';
 import { connectDB } from './config/db.js';
 
@@ -11,9 +12,11 @@ const PORT = ENV_VARS.PORT;
 app.use(express.json()); //will allow us to pars req.body
 
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/movie", movieRoutes);
 
 
 app.listen(PORT, ()=>{
       console.log("Server started at http://localhost:" + PORT);     
       connectDB();
 });
+
